d8f69f24106df875b778668dcd7262639f6781be
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2015-06-24 10:38:08 +0000

    Add squid access_log viewer container, add squid debugging

--- sample-client-loop.sh
+++ sample-client-loop.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 while true ; do
+    date --rfc-3339='seconds'
+    curl -v --proxy localhost:3128 http://web:5000/
+    echo "\n\n\n"
     echo waiting for 10 sec...
-    sleep 10 && curl -v --proxy localhost:3128 http://web:5000/
+    sleep 10
+    echo "\n\n\n"
 done
