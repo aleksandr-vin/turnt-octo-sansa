14cb53abba6a48e7e3325d1eed95a4b1a577b758
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2015-06-25 06:42:37 +0000

    Add no-cache

--- app/views.py
+++ app/views.py
@@ -4,5 +4,6 @@ from flask import session, request, make_response
 @app.route('/')
 def main_page():
     response = make_response(u'Boom!')
+    response.cache_control.no_cache = True
     response.cache_control.max_age = 40
     return response
