21b4f5c8763122167ce50cc9f55e275bcc2fd10a
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2015-07-01 10:41:17 +0000

    Add varnish

--- app/views.py
+++ app/views.py
@@ -3,7 +3,23 @@ from flask import session, request, make_response
 
 @app.route('/')
 def main_page():
-    response = make_response(u'Boom!')
+    etag = '123qwe321sss'
+
+    response = make_response(u'Boomzzzzzzzzzzzzzzzzxx!')
+    response.headers['ETag'] = etag
+    response.headers['Last-Modified'] = 'Mon, 29 Jun 2015 01:02:03 GMT'
+
     response.cache_control.no_cache = True
-    response.cache_control.max_age = 40
+#    response.cache_control.no_store = True
+    response.cache_control.max_age = 100
+    response.cache_control.no_transform = True
+    response.cache_control.must_revalidate = True
+#    response.cache_control.private = True
+#    response.cache_control.proxy_revalidate = True
+    response.cache_control.public = True
+#    response.cache_control.s_maxage = 100
+
+    if request.headers.get('If-None-Match') == etag:
+        return response, 304
+
     return response
--- sample-client-loop.sh
+++ sample-client-loop.sh
@@ -2,9 +2,9 @@
 
 while true ; do
     date --rfc-3339='seconds'
-    curl -v --max-time 5 --proxy localhost:3128 http://web:5000/
-    echo "\n\n\n"
+    curl -v --max-time 5 --proxy localhost:3128 http://web:5000/ #-H 'If-None-Match: 123qwe321'
+    printf "\n\n\n"
     echo waiting for 10 sec...
     sleep 10
-    echo "\n\n\n"
+    printf "\n\n\n"
 done
